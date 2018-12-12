7be2c3aa1365edf87ad46795e0bdfb94a5193f8a
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2018-12-12 13:16:36 +0700

    simple sub test example

--- main_test.go
+++ main_test.go
@@ -2,27 +2,12 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
-	"strconv"
 	"testing"
 )
 
 func TestGoroutines(t *testing.T) {
-	for i := 0; i < 5; i++ {
-		var name = "test #" + strconv.Itoa(i)
-		t.Run(name, func(t *testing.T) {
-			t.Error("start", name)
-			Sleep(name)
-			for i := 0; i < 5; i++ {
-				var name = "sub test #" + strconv.Itoa(i) + " from " + name
-				t.Run(name, func(t *testing.T) {
-					t.Error("start", name)
-					Sleep(name)
-					t.Error("end", name)
-					assert.Equal(t, "success "+name, "error "+name)
-				})
-			}
-			t.Error("end", name)
-			assert.Equal(t, "success "+name, "error "+name)
-		})
-	}
+	t.Run("sub test", func(t *testing.T) {
+		assert.Equal(t, "success sub test", "error sub test")
+	})
+	assert.Equal(t, "success main test", "error main test")
 }
