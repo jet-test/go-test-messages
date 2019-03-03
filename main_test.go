b4efde218268d86ad269dc34a16542b829f36dee
Aleksey Dobrynin <aleksey.dobrynin@jetbrains.com>
2019-03-03 19:40:30 +0700

    prepare demo tests

--- main_test.go
+++ main_test.go
@@ -3,11 +3,55 @@ package main
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
-func TestGoroutines(t *testing.T) {
-	t.Run("sub test", func(t *testing.T) {
-		assert.Equal(t, "success sub test", "error sub test")
+func TestSimpleSuccess(t *testing.T) {
+	t.Log("Success message")
+}
+
+func TestSimpleFail(t *testing.T) {
+	t.Error("Error message")
+	t.Fail()
+}
+
+func TestComparisonFail(t *testing.T) {
+	assert.Equal(t, "Expected message", "Actual message")
+}
+
+func TestSubTestsSuccess(t *testing.T) {
+	t.Log("Main test message")
+
+	t.Run("sub test #1", func(t *testing.T) {
+		t.Log("sub message #1")
 	})
-	assert.Equal(t, "success main test", "error main test")
+
+	t.Run("sub test #2", func(t *testing.T) {
+		t.Log("sub message #2")
+	})
+
+	t.Run("sub test #3", func(t *testing.T) {
+		t.Log("sub message #3")
+	})
+}
+
+func TestSubTestsFail(t *testing.T) {
+	t.Log("Main test first message")
+
+	t.Run("sub test #1", func(t *testing.T) {
+		t.Log("first message #1")
+		time.Sleep(time.Second)
+		t.Log("second message #1")
+		t.Fail()
+	})
+
+	t.Run("sub test #2", func(t *testing.T) {
+		t.Log("first sub message #2")
+		time.Sleep(time.Second)
+		t.Log("second sub message #2")
+		t.Fail()
+	})
+
+	t.Log("Main test second message")
+	t.Fail()
 }
